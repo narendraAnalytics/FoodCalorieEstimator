c2d3d33f1894f21a2d24c567afa37dcc8e1278f6
Narendra <narendra.insights@gmail.com>
2025-04-14 04:09:06 +0000

    Created app.py file

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ matplotlib.use('Agg')
 
 
 # Load environment variables
-load_dotenv()
-api_key = os.getenv('GEMINI_API_KEY')
-
 #load_dotenv()
-#api_key = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
+#api_key = os.getenv('GEMINI_API_KEY')
+
+load_dotenv()
+api_key = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
 
 
 # Check if the API key is loaded correctly
